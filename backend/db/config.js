d43d7f1fb53d49cbfcfb8329bdf2cd8198e355fd
Neus Garcia Puigdollers <neusgp@Neuss-MacBook-Pro.local>
2024-12-17 17:30:23 +0100

    note: add reminder

--- backend/db/config.js
+++ backend/db/config.js
@@ -4,6 +4,7 @@ import { Sequelize } from "sequelize";
 dotenv.config();
 const { HOST, DB_USER, DB_NAME, PASSWORD, PORT } = process.env;
 
+//to-do: connect to local db while developing!!
 export const sequelize = new Sequelize(DB_NAME, DB_USER, PASSWORD, {
   host: HOST,
   port: PORT,
