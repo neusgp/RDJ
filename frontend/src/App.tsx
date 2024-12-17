d189f568f1697b66cbb9f00aa4abf4b2a25e0d4b
Neus Garcia Puigdollers <neusgp@Neuss-MacBook-Pro.local>
2024-12-17 22:42:18 +0100

    style: landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./index.css";
 import { LandingPage } from "./pages/LandingPage";
 
 
@@ -7,9 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p className='text-red'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <LandingPage />
       </header>
     </div>
--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -2,6 +2,17 @@ import React from "react";
 
 export const LandingPage = () => {
     return (
-        <div className='text-slate-200 bg-red-400'>This is the Landing Page</div>
-    )
-}
+        <div className="flex flex-col bg-red-200 h-screen items-center justify-center gap-20">
+            <div className='flex flex-col items-center gap-4'>
+                <h1>Your Roller Derby journey doesn't stop when you leave the track.</h1>
+                <p className='w-[60%] text-center'>
+                    Now you can keep your Roller Derby notes in one place. Roller Derby Journal will help you
+                    organize your thoughts, ambitions, hopes and dreams about your
+                    favourite sport, so that you can give your best in your next derby challenge!
+                </p>
+            </div>
+            Hello!!
+            {/* <LoginForm /> */}
+        </div>
+    );
+};
