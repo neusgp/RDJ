a67108aa5b7f4e0b6d2837c0f5082d51ffb560dd
Neus Garcia Puigdollers <neusgp@Neuss-MacBook-Pro.local>
2024-12-17 14:31:39 +0100

    feat: connect to db with sequelize

--- backend/database.js
+++ backend/database.js
@@ -1,19 +1,16 @@
 import dotenv from "dotenv";
-import Pool from "pg";
+import { Sequelize } from "sequelize";
 
 dotenv.config();
 const { HOST, DB_USER, DB_NAME, PASSWORD, PORT } = process.env;
 
-export const db = new Pool({
+const sequelize = new Sequelize(DB_NAME, DB_USER, PASSWORD, {
   host: HOST,
-  user: DB_USER,
-  database: DB_NAME,
-  password: PASSWORD,
   port: PORT,
-  connectionTimeoutMillis: 5000,
-  idleTimeoutMillis: 10000,
-  max: 10,
-  ssl: {
-    connectionUnauthorized: false,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      connectionUnauthorized: false,
+    },
   },
 });
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
+import cors from "cors";
 import db from "./database";
-
-const cors = require("cors");
 
 const app = express();
 app.use(cors());
