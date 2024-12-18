97a18691a93a585ad184a2982af0f130350fced9
Neus Garcia Puigdollers <neusgp@Neuss-MacBook-Pro.local>
2024-12-18 11:34:43 +0100

    refactor: implement routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "./index.css";
 import { LandingPage } from "./pages/LandingPage";
-
+import { PasswordRecovery } from "./pages/PasswordRecovery";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <LandingPage />
-      </header>
+      <header className="App-header"></header>
+      <main>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/recovery" element={<PasswordRecovery />} />
+        </Routes>
+      </main>
     </div>
   );
 }
