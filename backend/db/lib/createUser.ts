bc2630ac21487a95b5a133a48c448d71ab541252
Neus Garcia Puigdollers <neusgp@Neuss-MacBook-Pro.local>
2024-12-19 13:07:22 +0100

    refactor: createUser

--- backend/db/lib/createUser.ts
+++ backend/db/lib/createUser.ts
@@ -1,4 +1,5 @@
 import { User, UserValidation } from "../models";
+import bcrypt from "bcrypt";
 
 export const createUser = async ({
   email,
@@ -7,21 +8,14 @@ export const createUser = async ({
   email: string;
   password: string;
 }) => {
-  //validation
-  //FIX
   UserValidation.parse({ email, password });
 
-  //make sure username is unique
   const existentUser = await User.findOne({ where: { email } });
   if (!!existentUser) throw new Error("Email is already in use");
 
-  //insert to db
-  //TO-DO: HASH PASWORD
-  await User.create({ email, password });
-  const newUser = await User.findOne({ where: { email, password } });
-  if (!newUser) throw new Error("Failed to store new user");
+  const hashedPassword = bcrypt.hashSync(password, 10);
 
-   //return user id
-  return newUser.dataValues.id;
-
+  await User.create({ email, password: hashedPassword });
 };
+
+
