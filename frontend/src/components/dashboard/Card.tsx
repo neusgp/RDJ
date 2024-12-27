69ec98cf61607fbddc2710d4962e42d896314f9d
Neus Garcia Puigdollers <neusgp@Neuss-MacBook-Pro.local>
2024-12-27 21:53:52 +0100

    style: rounded corners

--- frontend/src/components/dashboard/Card.tsx
+++ frontend/src/components/dashboard/Card.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Card = () => {
-  return <div className="border border-2 h-[200px] border-round"></div>;
+  return <div className="border border-2 h-[200px] rounded-lg"></div>;
 };
