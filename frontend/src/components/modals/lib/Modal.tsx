78e4a7f2883c48f521681634b4831091ca81e506
Neus Garcia Puigdollers <neusgp@Neuss-MBP.fritz.box>
2025-03-12 15:41:21 +0100

    feat: re-style modal component

--- frontend/src/components/modals/lib/Modal.tsx
+++ frontend/src/components/modals/lib/Modal.tsx
@@ -1,4 +1,5 @@
 import React, { JSX } from "react";
+import { CloseButton } from "../../buttons";
 
 export const Modal = ({
   children,
@@ -13,15 +14,10 @@ export const Modal = ({
     <div
       className="left-0 top-0 bg-black opacity-40 absolute z-10 h-screen w-[100%]"
       onClick={() => onClose && onClose()}></div>
-    <div className="absolute w-[40%] h-min-content bg-white z-20 right-[50%] top-[50%] translate-y-[-50%] translate-x-[50%] rounded-lg flex flex-col py-4 px-6 gap-8">
+    <div className="absolute w-[660px] h-min-content bg-white z-20 right-[50%] top-[50%] translate-y-[-50%] translate-x-[50%] rounded-[30px] flex flex-col py-6 px-10 gap-8">
       <div className="flex justify-between">
-        <p className="font-bold">{modalTitle}</p>
-        {/* todo: thsi should be an icon */}
-        {!!onClose && (
-          <p className="cursor-pointer" onClick={() => onClose()}>
-            x
-          </p>
-        )}
+        <p className="modal-title">{modalTitle}</p>
+        {!!onClose && <CloseButton onClose={onClose} />}
       </div>
       {children}
     </div>
