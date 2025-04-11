720ee989bddc7018069fa319b81045cec69b743f
Neus Garcia Puigdollers <neuspiano@gmail.com>
2025-04-11 19:01:52 +0200

    feat: implement notificationwith react portal

--- frontend/src/components/dashboard/ProfileDetails.tsx
+++ frontend/src/components/dashboard/ProfileDetails.tsx
@@ -1,18 +1,33 @@
 import React, { useState } from "react";
 import { ProfileDetailsModal } from "../modals";
+import { Notification } from "../informative/Notification";
 
 export const ProfileDetails = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isProfileUpdated, setIsProfileUpdated] = useState<boolean>(false);
 
   const handleIsOpen = () => setIsOpen(!isOpen);
 
+  const handleShowNotification = () => {
+    setIsProfileUpdated(true);
+  };
+
   return (
     <>
-      {/* todo: here goes a person icon */}
       <p className="cursor-pointer" onClick={() => handleIsOpen()}>
         Profile
       </p>
-      {isOpen && <ProfileDetailsModal handleIsOpen={handleIsOpen} />}
+      {isOpen && (
+        <ProfileDetailsModal
+          handleIsOpen={handleIsOpen}
+          showNotification={handleShowNotification}
+        />
+      )}
+      {isProfileUpdated && (
+        <Notification removeNotification={() => setIsProfileUpdated(false)}>
+          Successfully updated profile details
+        </Notification>
+      )}
     </>
   );
 };
---
+++ frontend/src/components/informative/Notification.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import ReactDom from "react-dom";
+
+const NONE = "hidden";
+
+export const Notification = ({
+  children,
+  removeNotification,
+}: {
+  children: React.ReactNode;
+  removeNotification: () => void;
+}) => {
+  const portalRoot = document.getElementById("portal-root");
+
+  useEffect(() => {
+    setTimeout(() => {
+      removeNotification();
+    }, 6000);
+  });
+
+  if (!portalRoot) return false;
+
+  return ReactDom.createPortal(
+    <div
+      className={`absolute w-[200px] right-[-200px] top-2 rounded-l-md flex justify-center content-center text-center bg-black text-white text-sm bold p-4 notification`}>
+      {children}
+    </div>,
+    portalRoot
+  );
+};
