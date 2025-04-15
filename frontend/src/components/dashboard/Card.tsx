ec60bef01e83f161e59495ac20a935824ecda710
Neus Garcia Puigdollers <neuspiano@gmail.com>
2025-04-15 18:12:37 +0200

    performance: memoize Card component

--- frontend/src/components/dashboard/Card.tsx
+++ frontend/src/components/dashboard/Card.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-import { TypeBadge } from "../informative/TypeBadge";
-
-export const Card = () => {
+const CardComponent = () => {
   return (
     <div className="border border-slate-100 shadow-md h-[200px] rounded-lg"></div>
   );
 };
+
+export const Card = React.memo(CardComponent);
